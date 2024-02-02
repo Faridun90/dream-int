d4251c7936c9249a53dbb0bcdc19768a84e7798b
Faridun Mirzoev <faridun.mirzoev@gmail.com>
2024-02-02 00:37:00 -0800

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+
+import React from "react"
+
+
+export function Navbar() {
+  return <div className=" gap-5 w-3/4">
+    <ul className="flex-row w-full">
+      <li>Discover More</li>
+      <li>Get Started</li>
+      <li>Explore Now</li>
+      <li>Menu</li>
+    </ul>
+  </div>
+}
