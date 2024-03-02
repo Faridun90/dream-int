cbbdc777a80e1f120ace0ae1136d11a094c41126
Faridun Mirzoev <faridun.mirzoev@gmail.com>
2024-03-01 16:02:32 -0800

    configure TS

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import heroImage from "../public/dream-hero.png";
+import heroImage from "@/public/dream-hero.png";
 import React from "react";
 
 const Hero = () => {
