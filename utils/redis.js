abe4d2e86e98919d82a7b31d48e6ebd09e6ecbbf
Faridun Mirzoev <faridun.mirzoev@gmail.com>
2024-10-07 22:41:09 -0700

    update redis credentials

--- utils/redis.js
+++ utils/redis.js
@@ -1,12 +1,16 @@
-import Redis from "ioredis";
+// utils/redis.js
+
+const Redis = require("ioredis");
 
 const redisConfig = {
-  host: REDIS_HOST || "localhost",
-  port: REDIS_PORT || 6379,
-  password: REDIS_PWD,
-  maxRetriesPerRequest: null,
+  // Redis server host
+  host: process.env.REDIS_HOST || "localhost",
+  // Redis server port
+  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+  // Redis password
+  password: process.env.REDIS_PASSWORD, // Ensure this environment variable is set
 };
-console.log(redisConfig);
+
 // Create a new Redis client instance
 const redisConnection = new Redis(redisConfig);
 
@@ -16,4 +20,4 @@ redisConnection.on("error", (error) => {
 });
 
 // Export the Redis client instance
-export default redisConnection;
+module.exports = redisConnection;
